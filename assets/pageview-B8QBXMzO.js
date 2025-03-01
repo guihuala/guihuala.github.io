c578b36474e35da3e4c7d9552f3784ffff17759f
小桂花 <143058334+guihuala@users.noreply.github.com>
2025-03-02 01:24:25 +0800

    deploy

---
+++ assets/pageview-B8QBXMzO.js
@@ -0,0 +1,7 @@
+import{a9 as t}from"./app-rFWVviJ8.js";var o={provider:"Waline",dark:'html[data-theme="dark"]',serverURL:"https://www.comment-waline.fun/",reaction:["https://unpkg.com/@waline/emojis@1.1.0/weibo/weibo_smile.png","https://unpkg.com/@waline/emojis@1.1.0/weibo/weibo_blush.png","https://unpkg.com/@waline/emojis@1.1.0/weibo/weibo_shuai.png","https://unpkg.com/@waline/emojis@1.1.0/weibo/weibo_sad.png"]};const n=async()=>{try{const{pageviewCount:e}=await t(()=>import("./app-rFWVviJ8.js").then(i=>i.aa),__vite__mapDeps([]));return e({serverURL:o.serverURL})}catch{console.error("@waline/client is not installed!");return}};export{n as updatePageview};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = []
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
